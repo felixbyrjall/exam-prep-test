466e2bdac9d2035edbfd59492aaad4691eac3f8d
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-17 16:04:14 +0100

    Created a start page with links to other pages

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {TodoApplication} from "./application";
+import "./application.css";
+import {BrowserRouter, Link} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<TodoApplication />);
+
+function Application(){
+    return (
+        <>
+            <header>
+                <h1>User database</h1>
+            </header>
+            <nav>
+                <Link to={"/"}>Front page</Link>
+                <Link to={"/"}>Another page</Link>
+                <Link to={"/"}>Yet one more page</Link>
+                <Link to={"/"}>Event more page</Link>
+                <Link to={"/"}>Event more page</Link>
+                <div className={"divider"} />
+                <Link to={"/login"}>Log in</Link>
+            </nav>
+            <main>
+                <Routes>
+                    <Route path={"/"} element={<h2>Front page</h2>} />
+                    <Route path={"*"} element={<h2>Not Found</h2>} />
+                </Routes>
+            </main>
+            <footer>Lecture 9: Open ID Connect</footer>
+        </>
+    );
+}
+
+
+root.render(
+    <BrowserRouter>
+        <Application />
+    </BrowserRouter>
+)
+//root.render(<TodoApplication />);
