5a3df2b8626feb25803e760ae6c4dee0d7306a59
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-17 18:33:24 +0100

    ListMovies now display Oppenheimer as default, and other titles submitted through add movies

--- client/index.jsx
+++ client/index.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 import "./application.css";
 import {BrowserRouter, Link} from "react-router-dom";
-import {MoviesRoutes} from "./components/moviesRoutes";
+import {MoviesRoutes} from "./components/movies/moviesRoutes";
+import {MoviesContext} from "./components/movies/moviesContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Application(){
+    const [movies, setMovies] = useState(
+        [
+            {
+                title: "Oppenheimer",
+            }
+        ]
+    )
+    function fetchMovies(){
+        return movies;
+    }
+
+    function onAddMovie(movie){
+        setMovies((oldMovies) => [... oldMovies, movie]);
+    }
+
     return (
-        <>
+        <MoviesContext.Provider value={{fetchMovies, onAddMovie}}>
             <header>
                 <h1>Movie database</h1>
             </header>
@@ -25,7 +41,7 @@ function Application(){
                 <MoviesRoutes/>
             </main>
             <footer>Lecture 9: Open ID Connect</footer>
-        </>
+        </MoviesContext.Provider>
     );
 }
 
