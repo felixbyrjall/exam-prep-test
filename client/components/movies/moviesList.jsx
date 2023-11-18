08220bb14dfb0eddf9a4b0a62708381e87ef2538
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-18 13:46:29 +0100

    Added a "loading" feature when the movies are loading

--- client/components/movies/moviesList.jsx
+++ client/components/movies/moviesList.jsx
@@ -2,10 +2,15 @@ import React, {useContext, useEffect, useState} from "react";
 import {MoviesContext} from "./moviesContext";
 
 export function MoviesList() {
-    const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     const {fetchMovies} = useContext(MoviesContext)
+
     async function loadMovies(){
+        setLoading(true);
         setMovies(await fetchMovies());
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -14,9 +19,10 @@ export function MoviesList() {
 
     return (
         <>
-            <h2>List movies</h2>
+            <h2>List the movies</h2>
+            {loading && <div>Spinner</div>}
             {movies.map((m) => (
-                <div>{m.title}</div>
+                <div key={m._id}>{m.title}</div>
             ))}
         </>
     )
