328f20d6baa068b6ed62b0ac075a17a5ed7b317b
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-18 16:43:19 +0100

    eslint config files with plugins

---
+++ server/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb", "prettier"],
+  ignorePatterns: ["jest.config.js"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@stylistic", "prettier"],
+  rules: {
+    "@stylistic/quotes": ["error", "double"],
+    "prettier/prettier": ["error"],
+  },
+};
