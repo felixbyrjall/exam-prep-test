2361db119cea1bd1aa3e8b94f28c00782152c04c
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-18 16:42:48 +0100

    added a loginpage route

--- client/components/movies/moviesRoutes.jsx
+++ client/components/movies/moviesRoutes.jsx
@@ -1,15 +1,17 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import React from "react";
-import {MoviesList} from "./moviesList";
-import {AddMovieForm} from "./addMovieForm";
+import { MoviesList } from "./moviesList";
+import { AddMovieForm } from "./addMovieForm";
+import { LoginPage } from "../login/loginPage";
 
 export function MoviesRoutes() {
-    return (
-        <Routes>
-            <Route path={"/"} element={<h2>Front page</h2>}/>
-            <Route path={"/movies"} element={<MoviesList/>}/>
-            <Route path={"/movies/new"} element={<AddMovieForm/>}/>
-            <Route path={"*"} element={<h2>404 not found</h2>}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path={"/"} element={<h2>Front page</h2>} />
+      <Route path={"/movies"} element={<MoviesList />} />
+      <Route path={"/movies/new"} element={<AddMovieForm />} />
+      <Route path={"/login"} element={<LoginPage />} />
+      <Route path={"*"} element={<h2>404 not found</h2>} />
+    </Routes>
+  );
 }
