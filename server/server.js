31f6e6d00fb61b82b85efb45ade1b5e926fcada2
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-19 12:49:24 +0100

    add cookie secret to signed cookie

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import * as path from "path";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
+// eslint-disable-next-line no-undef
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use((req, res, next) => {
-  const { username } = req.cookies;
+  const { username } = req.signedCookies;
   req.user = { username };
   next();
 });
 
 const loginRouter = express.Router();
 loginRouter.post("", (req, res) => {
-  res.cookie("username", req.body.username);
+  res.cookie("username", req.body.username, { signed: true });
   res.sendStatus(204);
 });
 
