eef74eb11e28c229913fadaf5d969e0998eb6371
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-19 22:58:05 +0100

    renamed onAddMovie in moviesContext

--- client/components/movies/moviesContext.jsx
+++ client/components/movies/moviesContext.jsx
@@ -2,5 +2,5 @@ import React from "react";
 
 export const MoviesContext = React.createContext({
   fetchMovies: async () => [],
-  onAddMovie: async (movie) => {},
+  postNewMovie: async (movie) => {},
 });
