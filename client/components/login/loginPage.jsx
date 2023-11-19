0833fea2b7c6ce706933c4c8b4d682fab99f4421
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-19 11:38:20 +0100

    Login with cookie

--- client/components/login/loginPage.jsx
+++ client/components/login/loginPage.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function LoginPage() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const res = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      throw new Error("Something went wrong! err: " + res.statusText);
+    }
+    navigate("/");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login Page</h2>
       <div>
-        <input type="text" />
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
       <div>
-        <input type="text" />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
+      <div>
+        <button>Log in</button>
+      </div>
+      <pre>{JSON.stringify({ username, password })}</pre>
     </form>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import * as path from "path";
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+const loginRouter = express.Router();
+loginRouter.post("", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.sendStatus(204);
+});
+
+app.use("/api/login", loginRouter);
+
 app.use(express.static("../client/dist"));
 
 // Express middleware for default route
@@ -14,33 +27,3 @@ app.use((req, res, next) => {
 });
 
 app.listen(process.env.PORT || 3000);
-
-/*let tasks = [
-    {id: 0, title: "wake up", status: "done"},
-    {id: 1, title: "do homework", status: "doing"}
-];
-
-app.get("/api/todos", (req, res) => {
-    res.json(tasks)
-});
-
-app.post("/api/todos", (req, res) => {
-    const {title} = req.body;
-    tasks.push({id: tasks.length, title, status: "todo"})
-    res.send();
-})
-
-app.put("/api/todos/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const {status} = req.body;
-    tasks = tasks.map(t => {
-        return t.id === id ? {...t, status: status} : t
-    });
-    res.send();
-});
-
-
-app.use(express.static(path.join("..", "client", "dist")));
-
-
-*/
