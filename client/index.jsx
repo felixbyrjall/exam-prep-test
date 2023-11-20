7bf1280361bc20d257f2e73a302dd4c7cc5c51b1
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-20 19:05:00 +0100

    moved browser router into application.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./application.css";
-import { BrowserRouter } from "react-router-dom";
+import "./resources/styles/styles.css";
 import { Application } from "./components/application/application";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <BrowserRouter>
-    <Application />
-  </BrowserRouter>,
-);
+root.render(<Application />);
