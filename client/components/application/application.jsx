48078dd92e0305ab16916fccba0bc5b485ece6ce
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-20 19:06:57 +0100

    moved moviesroutes into application, improved loadUser, and moved client_id into env

--- client/components/application/application.jsx
+++ client/components/application/application.jsx
@@ -1,39 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { LoginContext } from "../login/loginContext";
-import { MoviesContext } from "../movies/moviesContext";
-import { Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
 import { LoginNavLink } from "../login/loginNavLink";
-import { MoviesRoutes } from "../movies/moviesRoutes";
 import { LoginPage } from "../login/loginPage";
 import { ProfilePage } from "../profile/profilePage";
 import { LoginCallback } from "../login/loginCallback";
 
-const GOOGLE_CLIENT_ID =
-  "625916858663-tenc6s13hukrgg4tueqi62hg08v6o3ka.apps.googleusercontent.com";
+import { MoviesList } from "../movies/moviesList";
+import { AddMovieForm } from "../movies/addMovieForm";
+
+// eslint-disable-next-line no-undef
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
 export function Application() {
-  const [movies, setMovies] = useState();
-
-  async function fetchMovies() {
-    const res = await fetch("/api/movies");
-    return await res.json();
-  }
-
-  function postNewMovie(movie) {
-    setMovies((oldMovies) => [...oldMovies, movie]);
-  }
-
   const [username, setUsername] = useState();
   const [user, setUser] = useState();
 
   async function loadUser() {
-    const res = await fetch("/api/login");
-    if (!res.ok) {
-      throw new Error("Something went wrong fetching user " + res.statusText);
+    try {
+      const res = await fetch("/api/login");
+      if (!res.ok) {
+        throw new Error("Something went wrong fetching user " + res.statusText);
+      }
+      const userData = await res.json();
+      setUser(userData);
+      setUsername(userData.username);
+    } catch (error) {
+      console.error("Error loading user:", error);
     }
-    const user = await res.json();
-    setUser(user);
-    setUsername(user.username);
   }
 
   useEffect(() => {
@@ -49,12 +44,7 @@ export function Application() {
         client_id: GOOGLE_CLIENT_ID,
       }}
     >
-      <MoviesContext.Provider
-        value={{
-          fetchMovies,
-          postNewMovie,
-        }}
-      >
+      <BrowserRouter>
         <></>
         <header>
           <h1>Movie database</h1>
@@ -69,8 +59,10 @@ export function Application() {
           <LoginNavLink />
         </nav>
         <main>
-          <MoviesRoutes />
           <Routes>
+            <Route path={"/"} element={<h2>Front page</h2>} />
+            <Route path={"/movies"} element={<MoviesList />} />
+            <Route path={"/movies/new"} element={<AddMovieForm />} />
             <Route path={"/login"} element={<LoginPage />} />
             <Route path={"/login/callback"} element={<LoginCallback />} />
             <Route path={"/profile"} element={<ProfilePage />} />
@@ -78,7 +70,7 @@ export function Application() {
           </Routes>
         </main>
         <footer></footer>
-      </MoviesContext.Provider>
+      </BrowserRouter>
     </LoginContext.Provider>
   );
 }
