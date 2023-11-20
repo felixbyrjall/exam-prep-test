38362740aaa363d6708080e15c3f1bcc03712285
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-20 19:08:52 +0100

    moved fetchmovies into moviesList.jsx

--- client/components/movies/moviesList.jsx
+++ client/components/movies/moviesList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MoviesContext } from "./moviesContext";
 
 export function MoviesList() {
@@ -6,7 +6,13 @@ export function MoviesList() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
 
-  const { fetchMovies } = useContext(MoviesContext);
+  function postNewMovie(movie) {
+    setMovies((oldMovies) => [...oldMovies, movie]);
+  }
+  async function fetchMovies() {
+    const res = await fetch("/api/movies");
+    return await res.json();
+  }
 
   async function loadMovies() {
     setLoading(true);
@@ -28,11 +34,23 @@ export function MoviesList() {
 
   return (
     <>
-      <h2>List the movies</h2>
-      {loading && <div>Spinner</div>}
-      {movies.map((m) => (
-        <div key={m._id}>{m.title}</div>
-      ))}
+      <MoviesContext.Provider
+        value={{
+          fetchMovies,
+          postNewMovie,
+        }}
+      >
+        <h2>List the movies</h2>
+        {loading && <div>Spinner</div>}
+        {movies.map((m) => (
+          <div key={m._id}>
+            <h4>
+              {m.title} ({m.year})
+            </h4>
+            <p>{m.plot}</p>
+          </div>
+        ))}
+      </MoviesContext.Provider>
     </>
   );
 }
